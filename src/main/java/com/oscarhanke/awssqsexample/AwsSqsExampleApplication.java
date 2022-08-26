12673022b8f0cae07aed63c1b071763b50c980e9
ohanke <steamoha@wp.pl>
2022-08-26 12:04:06 +0200

    Initial commit

---
+++ src/main/java/com/oscarhanke/awssqsexample/AwsSqsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.awssqsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSqsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarhanke/awssqsexample/AwsSqsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.awssqsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

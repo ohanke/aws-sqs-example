0d47170338b0485af9bf24cef16265c69f9982b8
ohanke <steamoha@wp.pl>
2022-08-26 12:32:26 +0200

    required exclude, unclear why but wont work without it

--- src/main/java/com/oscarhanke/awssqsexample/AwsSqsExampleApplication.java
+++ src/main/java/com/oscarhanke/awssqsexample/AwsSqsExampleApplication.java
@@ -2,8 +2,16 @@ package com.oscarhanke.awssqsexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				ContextStackAutoConfiguration.class,
+				ContextInstanceDataAutoConfiguration.class,
+				ContextRegionProviderAutoConfiguration.class
+		})
 public class AwsSqsExampleApplication {
 
 	public static void main(String[] args) {
